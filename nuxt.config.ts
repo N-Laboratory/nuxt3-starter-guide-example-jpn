164972de35fedf5c2806222177f67586d1161b29
N-Laboratory <xnbaxx@gmail.com>
2023-02-02 14:34:39 +0900

    chore: install TypeScript

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  typescript: {
+    shim: false, // shimsファイル生成の無効化（VSCodeでVolarを使う場合はfalseにする）
+    strict: true, // 型チェックを厳格化
+    typeCheck: true // nuxt devまたはnuxt build時に型チェックを実行
+  }
 })
