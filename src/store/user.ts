4da3ed9d85a6acf3cb30c4a85b6451d713e90051
N-Laboratory <xnbaxx@gmail.com>
2024-12-08 20:11:25 +0900

    refactor: do lint fix

--- src/store/user.ts
+++ src/store/user.ts
@@ -4,13 +4,13 @@ import { defineStore } from 'pinia'
 export const useUserStore = defineStore('user', {
   state: () => ({
     // ユーザーの定義・初期化
-    user: { email: '', password: '' }
+    user: { email: '', password: '' },
   }),
   actions: {
     // ユーザー情報の更新
-    setUserInfo (email: string, password: string) {
+    setUserInfo(email: string, password: string) {
       this.user.email = email
       this.user.password = password
-    }
-  }
+    },
+  },
 })
